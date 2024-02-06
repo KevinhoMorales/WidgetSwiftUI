5a43481812bca288ca12339851e009e7da7a970a
Kevinho Morales <kfmorales94@gmail.com>
2024-02-05 22:35:10 -0500

    Project done

---
+++ WidgetSwiftUI/WidgetSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetSwiftUIApp.swift
+//  WidgetSwiftUI
+//
+//  Created by Kevinho Morales on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct WidgetSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WidgetTest/WidgetTestBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetTestBundle.swift
+//  WidgetTest
+//
+//  Created by Kevinho Morales on 5/2/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetTestBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetTest()
+        WidgetTestLiveActivity()
+    }
+}
